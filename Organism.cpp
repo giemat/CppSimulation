a0609045a6056029a911e175df096d1eb634034f
Mateusz <s197714@student.edu.pg.pl>
2024-04-14 18:17:52 +0200

    organism outline

--- Organism.cpp
+++ Organism.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Organism.h"
+Organism::Organism(int _strength, int _initiative, World *_world_ptr)
+    :strength(_strength), initiative(_initiative), world_ptr(_world_ptr) {}
+
+int Organism::getStrength() const {
+    return strength;
+}
+
+int Organism::getInitiative() const {
+    return initiative;
+}
+
+int Organism::getPosition() const {
+    return *pos;
+}
+
+void Organism::changePosition(int x, int y) {
+    this->pos[0] = x;
+    this->pos[1] = y;
+}
--- Organism.h
+++ Organism.h
@@ -4,10 +4,21 @@
 
 #ifndef PO_P1_ORGANISM_H
 #define PO_P1_ORGANISM_H
-
+#include "World.h"
 
 class Organism {
-
+private:
+    int strength;
+    int initiative;
+    int pos[2];
+    World* world_ptr;
+public:
+    Organism(int _strength, int _initiative, World *_world_ptr);
+    ~Organism() = default;
+    int getStrength() const;
+    int getInitiative() const;
+    int getPosition() const;
+    void changePosition(int x, int y);
 };
 
 
